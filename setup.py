c6fd249186e3cafb38097be86646ff073b5241f1
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2016-07-13 00:17:21 +0000

    add licence and build/install script

---
+++ setup.py
@@ -0,0 +1,21 @@
+#+
+# Distutils script to install PyBidi. Invoke from the command line
+# in this directory as follows:
+#
+#     python3 setup.py install
+#
+# Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+#-
+
+import distutils.core
+
+distutils.core.setup \
+  (
+    name = "PyBidi",
+    version = "0.9",
+    description = "language bindings for FriBidi, for Python 3.3 or later",
+    author = "Lawrence D'Oliveiro",
+    author_email = "ldo@geek-central.gen.nz",
+    url = "http://github.com/ldo/pybidi",
+    py_modules = ["fribidi"],
+  )
