d2f43cc85f9c486a2594575ffe0d6190a429bb53
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2023-09-27 23:45:17 +0000

    Update public repo locations

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup \
     description = "language bindings for FriBidi, for Python 3.3 or later",
     author = "Lawrence D'Oliveiro",
     author_email = "ldo@geek-central.gen.nz",
-    url = "https://github.com/ldo/pybidi",
+    url = "https://gitlab.com/ldo/pybidi",
     py_modules = ["fribidi"],
   )
